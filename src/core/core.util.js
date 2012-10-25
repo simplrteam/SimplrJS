cea6cb754dd4ce4926e0393e7ba394f8147776fd
Brian Ritter <ritterb82@gmail.com>
2012-10-24 22:32:38 -0400

    re-added as "core"

---
+++ src/core/core.util.js
@@ -0,0 +1,102 @@
+(function() {
+	
+	var hasLocalStorage = typeof localStorage == "undefined" ? false : true;
+	
+	function htmlEntities(string) {
+		return string.replace(/&/g,'&amp;').replace(/</g,'&lt;').replace(/>/g,'&gt;').replace(/"/g,'&quot;');
+	};
+	
+	function equal(thing1, thing2) {
+		if( typeof thing1 == typeof thing2 ) {
+			if(typeof thing1 == "object") {
+				if( $.isArray(thing1) ) {
+					if( thing1.length == thing2.length ) {
+						for(var i = 0, iL = thing1.length; i < iL; i++) {
+							if( !equal(thing1[i], thing2[i]) ) { 
+								return false; 
+							}
+						}
+						return true;
+					}
+					return false;
+				} else {
+					var ret = true;
+					for(var key in thing1) {
+						if( !equal(thing1[key], thing2[key]) ) { 
+							ret = false; return false; 
+						}
+					}
+					if(ret) {
+						var l1 = 0;
+						var l2 = 0;
+						for(var key in thing1) {
+							l1++;
+						}
+						for(var key in thing2) {
+							l2++;
+						}
+						return l1 == l2;
+					}
+					return false;
+				}
+			} 
+			return thing1 == thing2; 
+		}
+		return false;
+	};
+	
+	Simplr.Core.Util = {
+		mEmpty : function(thing) {
+			var typeOfThing = typeof thing;
+			if( typeOfThing != "undefined" && thing != null ) {
+				if(typeOfThing == "object") {
+					if($.isArray(thing)) {
+						return thing.length == 0;
+					}
+					return $.isEmptyObject(thing);
+				}
+				return $.trim(thing) == "";				
+			}
+			return true;
+		},
+		mEqual : function(things) {
+			if( !Simplr.Core.Util.mEmpty(things) ) {
+				if( $.isArray(things) ) {
+					var valid = true;
+					for(var i = 0, iL = things.length; i < iL; i++) {
+						if( !equal(things[0], things[i]) ) {
+							valid = false;
+							i = iL;
+						}
+					}
+					return valid;
+				} 
+				return true;
+			}
+			return true;
+		},
+		mGetUrlParameter : function(name) {
+			var string = window.location.search;
+			if(!Simplr.Core.Util.mEmpty(string)) {
+				var parameters = {};
+				string = string.substring(1).split("&");
+				for(var i = 0, iL = string.length; i < iL; i++) {
+					var keyValue = string[i].split("=");
+					parameters[keyValue[0]] = $.trim(htmlEntities(decodeURIComponent(keyValue[1])));
+				}
+				if(Simplr.Core.Util.mEmpty(name)) {
+					return parameters;
+				} else {
+					var value = parameters[name];
+					return typeof value == "undefined" ? null : value;
+				}
+			} else { 
+				return null;
+			}
+		},
+		mHasLocalStorage : function() {
+			return hasLocalStorage;
+		}
+	};
+	
+})();
---
+++ src/core/defaultCodesAndValidators.js
@@ -0,0 +1,100 @@
+/* Add Codes */
+Simplr.Core.Validation.mAddCodes({ 
+	eMissingValidator : "Missing Validator",
+	eAlphaNumeric : "$[label] is not alphanumeric.",
+	eAmericanExpress : "$[label] is not a valid AMERICAN EXPRESS number.",
+	eDinersClub : "$[label] is not a valid DINERS CLUB number.",
+	eDiscover : "$[label] is not a valid DISCOVER number.",
+	eEmail : "$[label] is not an email address.",
+	eEqual : "$[label] does not match.",
+	eMastercard : "$[label] is not a valid MASTERCARD number.",
+	eEmpty : "$[label] is empty.",
+	eNumber : "$[label] is not a number.",
+	ePhoneNumber : "$[label] is not a valid Phone Number.",
+	ePostalCode : "$[label] is not a Postal Code.",
+	eVisa : "$[label] is not a valid VISA number."
+});
+
+/* Add Validators */
+Simplr.Core.Validation.mAddValidators({
+	/* Missing Validator */
+	missingvalidator : function(value) { 
+		return $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : false, errorCodes : [ "eMissingValidator" ] }); 
+	},
+	
+	alphanumeric : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^\w*$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eAlphaNumeric"); }
+		return results;
+	},
+	americanexpress : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^3(4|7)\d{2}-?\d{6}-?\d{5}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eAmericanExpress"); }
+		return results;
+	},
+	dinersclub : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^3[0,6,8]\d{12}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eDinersClub"); }
+		return results;
+	},
+	discover : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^6011-?\d{4}-?\d{4}-?\d{4}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eDiscover"); }
+		return results;
+	},
+	email : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eEmail"); }
+		return results;
+	},
+	equal : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate());
+		if( $.isArray(value) ) { 
+			for(var i = 0, iL = value.length; i < iL; i++) {
+				if(!Simplr.Core.Util.mEqual([value[0], value[i]])) {
+					results.valid = false; 
+					i = iL;
+				}
+			}
+		}
+		if( !results.valid ) { results.errorCodes.push("eEqual"); }
+		return results;
+	},
+	mastercard : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^5[1-5]\d{2}-?\d{4}-?\d{4}-?\d{4}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eMastercard"); }
+		return results;
+	},
+	notempty : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate());
+		if( typeof value != "undefined" && value != null ) {
+			if( typeof value == "string" ) { results.valid = !($.trim(value) == ""); }
+			else if( $.isArray(value) ) { results.valid = !(value.length == 0); }
+			else if( typeof value == "object" ) { results.valid = !$.isEmptyObject(value); }
+		}
+		if(!results.valid) { results.errorCodes.push("eEmpty"); }
+		return results;
+	},
+	number : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : ( !isNaN(parseFloat(value)) && isFinite(value) ) });
+		if( !results.valid ) { results.errorCodes.push("eNumber"); }
+		return results;
+	},
+	phonenumber : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^\d{10}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("ePhoneNumber"); }
+		return results;
+	},
+	postalcode : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^\d{5}([\-]?\d{4})?$/.test(value) });
+		if( !results.valid ) { 
+			results.errorCodes.push("ePostalCode") 
+		}
+		return results;	
+	},
+	visa : function(value) {
+		var results = $.extend(true, {}, Simplr.Core.Validation.mGetRuleResultsTemplate(), { valid : /^4\d{3}-?\d{4}-?\d{4}-?\d{4}$/.test(value) });
+		if( !results.valid ) { results.errorCodes.push("eVisa"); }
+		return results;
+	}
+});
